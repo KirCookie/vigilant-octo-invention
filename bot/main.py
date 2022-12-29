7920d7ed9725f41e93c992f8e4aea999f46a123d
unknown <kircookie19@mail.ru>
2022-12-29 09:27:14 +0500

    v2

--- bot/main.py
+++ bot/main.py
@@ -8,14 +8,13 @@ TOKEN = os.getenv('TOKEN')
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
-def start(m, res=False):
-        bot.send_message(m.chat.id, 'Йоу, я на связи')
+def start(message):
+        bot.send_message(message.chat.id, '')
 
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     for i in range (1, 365):
-    # Отправка ответа
-        bot.send_message(message.chat.id, '/pidor')
+        bot.send_message(message.chat.id, '')
         time.sleep(86400)
-# Запускаем бота
+
 bot.polling(none_stop=True, interval=0)
